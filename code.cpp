d2c8f71280b03e01405d79b204df7d8aa16ee67f
Kle1n19 <115091987+Kle1n19@users.noreply.github.com>
2024-12-03 16:12:18 +0200

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,51 +1,92 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <iomanip>
 #include <cstring>
 
 using namespace std;
+
 uint16_t read_uint16(ifstream& file) {
     uint16_t value;
     file.read(reinterpret_cast<char*>(&value), sizeof(value));
     return value;
 }
+
 uint32_t read_uint32(ifstream& file) {
     uint32_t value;
     file.read(reinterpret_cast<char*>(&value), sizeof(value));
     return value;
 }
 
-uint8_t read_uint8(ifstream& file) {
-    uint8_t value;
-    file.read(reinterpret_cast<char*>(&value), sizeof(value));
-    return value;
-}
-void print_fat16_file_info(ifstream& file, uint16_t root_dir_start, uint16_t root_entries, uint16_t sector_size, uint8_t sectors_per_cluster) {
-    file.seekg(root_dir_start * sector_size, ios::beg);
+class FAT16 {
+public:
+    uint16_t sector_size;
+    uint8_t sectors_per_cluster;
+    uint16_t reserved_sectors;
+    uint8_t fat_count;
+    uint16_t root_entries;
+    uint16_t fat_size_sectors;
+    uint16_t root_dir_start;
+    uint16_t data_start;
+    vector<uint16_t> fat_table;
 
-    for (int i = 0; i < root_entries; ++i) {
+    FAT16(ifstream& file) {
+        file.seekg(11, ios::beg);
+        sector_size = read_uint16(file);
+        sectors_per_cluster = file.get();
+        reserved_sectors = read_uint16(file);
+        fat_count = file.get();
+        root_entries = read_uint16(file);
+        fat_size_sectors = read_uint16(file);
+
+        root_dir_start = reserved_sectors + fat_count * fat_size_sectors;
+        data_start = root_dir_start + (root_entries * 32 + sector_size - 1) / sector_size;
+
+        file.seekg(reserved_sectors * sector_size, ios::beg);
+        for (int i = 0; i < fat_size_sectors * sector_size / 2; ++i) {
+            fat_table.push_back(read_uint16(file));
+        }
+    }
+
+    uint16_t get_next_cluster(uint16_t cluster) const {
+        return fat_table[cluster];
+    }
+
+    uint32_t cluster_to_sector(uint16_t cluster) const {
+        return data_start + (cluster - 2) * sectors_per_cluster;
+    }
+};
+
+void print_file_info(const string& path, const string& name, uint32_t size, bool is_directory) {
+    cout << path << (path.empty() ? "" : "/") << name << (is_directory ? "/" : "") 
+         << " (Size: " << size << " bytes, Type: " 
+         << (is_directory ? "Directory" : "File") << ")" << endl;
+}
+
+void read_directory(ifstream& file, const FAT16& fat, uint16_t cluster, const string& path) {
+    uint32_t sector = fat.cluster_to_sector(cluster);
+    file.seekg(sector * fat.sector_size, ios::beg);
+
+    while (true) {
         char entry[32];
         file.read(entry, sizeof(entry));
-        if (entry[0] == 0x00) continue;
+        if (entry[0] == 0x00) break;
         if (entry[0] == 0xE5) continue;
-        string filename(entry, 8);
-        string ext(entry + 8, 3);
-        string full_name = filename + "." + ext;
-        uint32_t file_size = *reinterpret_cast<uint32_t*>(&entry[28]);
-        uint8_t attributes = entry[11];
-        bool is_directory = attributes & 0x10;
-        cout << "File: " << full_name << endl;
-        cout << "Size: " << file_size << " bytes" << endl;
-        cout << "Type: " << (is_directory ? "Directory" : "File") << endl;
-        cout << "Attributes: ";
-        if (attributes & 0x01) cout << "Read-Only ";
-        if (attributes & 0x02) cout << "Hidden ";
-        if (attributes & 0x04) cout << "System ";
-        if (attributes & 0x08) cout << "Volume Label ";
-        if (attributes & 0x10) cout << "Directory ";
-        if (attributes & 0x20) cout << "Archive ";
 
-        cout << endl;
+        string name(entry, 8);
+        string ext(entry + 8, 3);
+        uint8_t attributes = entry[11];
+        uint16_t start_cluster = *reinterpret_cast<uint16_t*>(&entry[26]);
+        uint32_t file_size = *reinterpret_cast<uint32_t*>(&entry[28]);
+
+        name.erase(name.find_last_not_of(' ') + 1);
+        ext.erase(ext.find_last_not_of(' ') + 1);
+        string full_name = name + (ext.empty() ? "" : "." + ext);
+        bool is_directory = attributes & 0x10;
+        print_file_info(path, full_name, file_size, is_directory);
+        if (is_directory && full_name != "." && full_name != "..") {
+            read_directory(file, fat, start_cluster, path + "/" + full_name);
+        }
     }
 }
 void read_fat16_image(const string& image_path) {
@@ -54,30 +95,16 @@ void read_fat16_image(const string& image_path) {
         cerr << "Could not open the file: " << image_path << endl;
         return;
     }
-    char boot_sector[512];
-    file.read(boot_sector, sizeof(boot_sector));
-    uint16_t sector_size = *reinterpret_cast<uint16_t*>(&boot_sector[11]);
-    uint8_t sectors_per_cluster = boot_sector[13];
-    uint16_t fat_count = boot_sector[16];
-    uint16_t fat_size_sectors = *reinterpret_cast<uint16_t*>(&boot_sector[22]);
-    uint16_t root_entries = *reinterpret_cast<uint16_t*>(&boot_sector[17]);
-    uint16_t reserved_sectors = *reinterpret_cast<uint16_t*>(&boot_sector[14]);
-    uint16_t root_dir_start = reserved_sectors + fat_count * fat_size_sectors;
-    uint32_t fat_size_bytes = fat_size_sectors * sector_size;
-    uint16_t signature = *reinterpret_cast<uint16_t*>(&boot_sector[510]);
-
+    FAT16 fat(file);
     cout << "FAT16 File System Information:" << endl;
-    cout << "Sector size: " << sector_size << " bytes" << endl;
-    cout << "Sectors per cluster: " << (int)sectors_per_cluster << endl;
-    cout << "Number of FAT copies: " << (int)fat_count << endl;
-    cout << "FAT size (in sectors): " << fat_size_sectors << endl;
-    cout << "FAT size (in bytes): " << fat_size_bytes << endl;
-    cout << "Root directory entries: " << root_entries << endl;
-    cout << "Reserved sectors: " << reserved_sectors << endl;
-
-    cout << "Valid boot signature: " << (signature == 0xAA55 ? "Yes" : "No") << endl;
-
-    print_fat16_file_info(file, root_dir_start, root_entries, sector_size, sectors_per_cluster);
+    cout << "Sector size: " << fat.sector_size << " bytes" << endl;
+    cout << "Sectors per cluster: " << (int)fat.sectors_per_cluster << endl;
+    cout << "Number of FAT copies: " << (int)fat.fat_count << endl;
+    cout << "FAT size (in sectors): " << fat.fat_size_sectors << endl;
+    cout << "Root directory entries: " << fat.root_entries << endl;
+    cout << "Reserved sectors: " << fat.reserved_sectors << endl;
+    cout << "\nRoot Directory:" << endl;
+    read_directory(file, fat, 0, "");
 
     file.close();
 }
